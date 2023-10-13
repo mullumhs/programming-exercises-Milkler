55d752b2f0d90d969baf3d4259588267a7fbdf40
Milkler <147568714+Milkler@users.noreply.github.com>
2023-10-13 12:01:01 +1100

    Update 01-HelloWorld.cs

--- 01-HelloWorld/01-HelloWorld.cs
+++ 01-HelloWorld/01-HelloWorld.cs
@@ -13,7 +13,8 @@ Main Tasks:
 
 Questions:
 1. What is the difference between input and output?
-
+Imput is Brom the person
+Output is from the computer
 Extension Tasks:
 1. Output another message / pause after the ReadLine.
 2. Research online how to change the colour and/or make the console beep.
@@ -31,7 +32,11 @@ namespace ProgrammingExercisesIST
         static void Main(string[] args)
         {
             // Your code goes below here
-
+            Console.WriteLine("Hey Kitty");
+            Console.ReadLine();
+            Console.WriteLine("Goodbye Kitty");
+            Console.Beep(10000,10000);
+            Console.ReadLine();
         }
     }
 }
@@ -40,7 +45,25 @@ namespace ProgrammingExercisesIST
 ===============================================================================
 FEEDBACK AREA: Paste your AI feedback here before submission:
 ===============================================================================
+Craig Feedback:
 
+Task 1: You have successfully used Console.WriteLine() to output a string to the console. However, the task was to output 'Hello World!', but you have used 'Hey Kitty'. Try to stick to the task requirements.
+
+Task 2: You have correctly used Console.ReadLine() to make the program wait for input before proceeding. Well done!
+
+Extension Task 1: You have successfully output another message after the Console.ReadLine(). Good job!
+
+Extension Task 2: You have correctly used Console.Beep() to make the console beep. However, the frequency and duration you have used (10000,10000) may result in a very loud and long beep. You might want to experiment with different values.
+
+Code Commenting Feedback:
+
+Your code could benefit from more comments, especially before each line of code explaining what it does. This is good practice and helps others understand your code.
+Question Feedback:
+
+Your answer to the question about the difference between input and output is not entirely accurate. Input refers to the data entered into the computer by the user, while output is the data that the computer returns after processing. Try to provide more detail in your answers.
+Overall:
+
+You're doing well! Just remember to follow the task requirements more closely and add more comments to your code. Keep practicing!
 
 
 ===============================================================================
